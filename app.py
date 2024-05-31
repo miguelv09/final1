3160c6f3fba28974f13ad202c78fdcc80b7377e3
miguelv09 <143090873+miguelv09@users.noreply.github.com>
2024-05-30 19:43:09 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import requests
+import pandas as pd
+import time
+
+st.title('Monitor de Temperatura y Humedad')
+
+temperature = st.empty()
+humidity = st.empty()
+chart_data = st.empty()
+
+data_points = []
+
+def get_data():
+    response = requests.get('http://')  
+    return response.json()
+
+while True:
+    data = get_data()
+    temp = data['temperature']
+    hum = data['humidity']
+
+    temperature.metric("Temperatura", f"{temp} °C")
+    humidity.metric("Humedad", f"{hum} %")
+
+    data_points.append([time.time(), temp, hum])
+    df = pd.DataFrame(data_points, columns=['Time', 'Temperature', 'Humidity'])
+
+    chart_data.line_chart(df.set_index('Time'))
+
+    time.sleep(2)
